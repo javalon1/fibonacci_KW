baf15d1014035ce0abfef86de252a41f979ceabb
Karolina Wanat <carolinawanat@gmail.com>
2019-05-12 10:52:53 +0100

    add fibonacci with variable and loop

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -2,26 +2,53 @@ package com.sda.algorytmy;
 
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
-        System.out.println(fibonacciR(5));
+        //System.out.println(fibonacciR(5));
 
-        System.out.println(Arrays.toString(fibonacciI(20)));
+        //System.out.println(Arrays.toString(fibonacciI(20)));
+
+        fibonacci2();
 
     }
 
-    private static int[] fibonacciI(int n){
+    private static void fibonacci2() {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int k = 2;
+
+        if (n < 2) {
+            System.out.println(n);
+        } else {
+            while (k < n) {
+                    a = b;
+                    b = c;
+                    c = a + b;
+
+                    k++;
+                }
+
+            }
+        System.out.println(c);
+        }
+
+
+    private static int[] fibonacciI(int n) {
 
 
         int[] tab = new int[n];
         tab[0] = 0;
         tab[1] = 1;
 
-        for (int i = 2; i < n ; i++) {
+        for (int i = 2; i < n; i++) {
 
-            tab[i] = (tab[i-1]) + (tab[i-2]);
+            tab[i] = (tab[i - 1]) + (tab[i - 2]);
         }
 
         return tab;
@@ -33,7 +60,7 @@ public class App {
             return 1;
         }
 
-        return fibonacciR(n-1) + fibonacciR(n-2);
+        return fibonacciR(n - 1) + fibonacciR(n - 2);
 
 
     }
